3bf890ffa8cd67359001443a555274c3a9853931
avengerweb <avengerweb@yandex.ru>
2020-06-19 16:54:52 -0700

    Reporting

--- app/Backup.php
+++ app/Backup.php
@@ -4,12 +4,14 @@ namespace App;
 
 
 use Carbon\Carbon;
+use GuzzleHttp\Client;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
 use Symfony\Component\Process\Process;
 
 class Backup
 {
+    const MAX_HOOK_TRIES = 3;
     var string $path;
     const FULL_BACKUP = "full_weekly";
 
@@ -70,6 +72,7 @@ class Backup
     private function createFullWeeklyBackup(): bool
     {
         $week = $this->path . DIRECTORY_SEPARATOR . $this->getWeekDirectory();
+        $target = $week . DIRECTORY_SEPARATOR . self::FULL_BACKUP;
 
         // mariabackup --backup --target-dir=/mnt/storage/mariadb/full1 --user=root
         $process = new Process([
@@ -86,6 +89,12 @@ class Backup
         $code = $process->run(function ($type, $buffer) {
             echo $buffer;
         });
+
+        try {
+            $this->sendReport($target, $code == 0, true);
+        } catch (\Exception $e) {
+            $this->log('Backup completed but report can be sent: '.$e->getMessage()."\n".$e->getTraceAsString());
+        }
 
         return $code == 0;
     }
@@ -108,12 +117,13 @@ class Backup
     private function createTodayBackup(): bool
     {
         $week = $this->path . DIRECTORY_SEPARATOR . $this->getWeekDirectory();
+        $target = $week . DIRECTORY_SEPARATOR . $this->today();
 
         // mariabackup --backup --target-dir=/mnt/storage/mariadb/inc1_3 --incremental-basedir=/mnt/storage/mariadb/full1 --user=root
         $process = new Process([
             'mariabackup',
             '--backup',
-            '--target-dir=' . $week . DIRECTORY_SEPARATOR . $this->today(),
+            '--target-dir=' . $target,
             '--incremental-basedir=' . $week . DIRECTORY_SEPARATOR . self::FULL_BACKUP,
             '--user=root'
         ]);
@@ -126,6 +136,69 @@ class Backup
             echo $buffer;
         });
 
+        try {
+            $this->sendReport($target, $code == 0, false);
+        } catch (\Exception $e) {
+            $this->log('Backup completed but report can be sent: '.$e->getMessage()."\n".$e->getTraceAsString());
+        }
+
         return $code == 0;
+    }
+
+    public function sendReport(string $pathToBackup, bool $isDone, bool $isFull): void
+    {
+        $url = $_ENV['BACKUP_HOOK_URL'] ?? null;
+        if (! $url) {
+            $this->log("No report will be sent, please set BACKUP_HOOK_URL");
+            return;
+        }
+
+        $fullPath = $this->path.DIRECTORY_SEPARATOR.$pathToBackup;
+
+        $report = [
+            'week' => $this->getWeekDirectory(),
+            'date' => $this->today(),
+            'is_full' => $isFull,
+            'success' => $isDone,
+            'space' => [
+                'free' => disk_free_space($fullPath),
+                'total' => disk_total_space($fullPath),
+            ],
+        ];
+
+        if ($isDone) {
+            try {
+                $report['meta'] = $this->parseMeta($pathToBackup.DIRECTORY_SEPARATOR.'xtrabackup_info');
+            } catch (\Exception $exception) {
+                $this->log("Couldn't parse meta information.");
+            }
+        }
+
+        $client = new Client();
+
+        $tries = 0;
+
+        while (true) {
+            try {
+                $client->post($_ENV['BACKUP_HOOK_URL'], ['json' => $report]);
+                break;
+            } catch (\Exception $exception) {
+                if ($tries > self::MAX_HOOK_TRIES) {
+                    throw $exception;
+                }
+
+                $tries++;
+            }
+        }
+    }
+
+    private function parseMeta(string $path): array
+    {
+        $content = $this->filesystem->get($path);
+
+        return array_map(function (string $line) {
+            $data = explode("=", $line, 2);
+            return ['key' => trim($data[0]), 'value' => trim($data[1] ?? "")];
+        }, explode("\n", $content->read()));
     }
 }
